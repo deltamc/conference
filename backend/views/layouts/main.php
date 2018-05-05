2f2c7dc4af49207493122a156c3c7c1bd472a77b
deltamc <biomars@yandex.ru>
2018-05-05 07:43:01 +0300

    Adding a link to the menu

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Учебные заведения', 'url' => ['/schools']],
     ];
     if (Yii::$app->user->isGuest) {
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
