0eaec92000af60f176d464a8065b061279853c9d
deltamc <biomars@yandex.ru>
2018-05-09 17:22:57 +0300

    Events table

--- backend/views/events/index.php
+++ backend/views/events/index.php
@@ -1,12 +1,14 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\grid\GridView;
+
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Events';
+$this->title = 'Мероприятия';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="events-index">
@@ -14,20 +16,59 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Events', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Добавить мероприятие', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'name',
+            [
+                'attribute'=>'name',
+                'format' => 'raw',
+                'value' => function($data){
+                    return Html::a(
+                        $data->name,
+                        Url::to(['update', 'id' => $data->id])
+                    );
+                }
+            ],
+            [
+                'label' => '',
+                'format' => 'raw',
+                'value' => function($data){
+                    return Html::a(
+                        'Секции',
+                        Url::to(['sections/', 'event'=>$data->id])
+                    );
+                }
+            ],
+            [
+                'label' => '',
+                'format' => 'raw',
+                'value' => function($data){
+                    return Html::a(
+                        'Участники',
+                        Url::to(['participants/', 'event' => $data->id])
+                    );
+                }
+            ],
             'dataStartReg:datetime',
             'dataEndReg:datetime',
-
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{update} {delete}',
+                'buttons' => [
+                    'delete' => function($url, $model, $key) {
+                        return Html::a(
+                            '<span class="glyphicon glyphicon-trash"></span>',
+                            Url::to(['delete', 'id' => $model->id]),
+                            [
+                                'data-confirm' => 'Удалить?',
+                            ]
+                        );
+                    }
+                ],
+            ],
         ],
     ]); ?>
 </div>
