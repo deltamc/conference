6fb1c767984ca14faa6b3002b677bf8383935a92
deltamc <biomars@yandex.ru>
2018-05-09 10:50:32 +0300

    DateTimePicker

--- backend/views/events/_form.php
+++ backend/views/events/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\datetime\DateTimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Events */
@@ -14,9 +15,26 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'dataStartReg')-> ?>
+    <?= $form->field($model, 'dataStartReg')->widget(DateTimePicker::class, [
+        'name' => 'dataStartReg',
+        //'options' => ['placeholder' => 'Select operating time ...'],
+        'convertFormat' => true,
+        'pluginOptions' => [
+            'format' => 'php:d.m.Y h:i:s',
+            'startDate' => date('d.m.Y h:i:s'),
+            'todayHighlight' => true
+    ]]); ?>
 
-    <?= $form->field($model, 'dataEndReg')->textInput() ?>
+
+    <?= $form->field($model, 'dataEndReg')->widget(DateTimePicker::class, [
+        'name' => 'dataEndReg',
+        //'options' => ['placeholder' => 'Select operating time ...'],
+        'convertFormat' => true,
+        'pluginOptions' => [
+            'format' => 'php:d.m.Y h:i:s',
+            'startDate' => date('d.m.Y h:i:s'),
+            'todayHighlight' => true
+        ]]); ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
