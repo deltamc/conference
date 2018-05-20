eec2b7f5da28cb605c7b013de20d2ca177401dd1
deltamc <biomars@yandex.ru>
2018-05-20 11:12:11 +0300

    Adding a model

---
+++ backend/models/Section.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+class Section extends \kartik\tree\models\Tree
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%sections}}';
+    }
+
+    public function getTypes()
+    {
+        return [
+            'section'    => 'Секция',
+            'subsection' => 'Подсекция',
+            'age_group'  => 'Возрастная группа'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        $rules = parent::rules();
+        $rules[] = ['type', 'string'];
+        return $rules;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        $attributes = parent::attributeLabels();
+        $attributes['type'] = 'Тип';
+        return $attributes;
+    }
+
+}
