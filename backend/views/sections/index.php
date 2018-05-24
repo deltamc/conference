055e7775c94ce4ddd602f48eb2306d92491fe7f0
deltamc <biomars@yandex.ru>
2018-05-24 07:48:29 +0300

    Deleted comments

--- backend/views/sections/index.php
+++ backend/views/sections/index.php
@@ -13,18 +13,16 @@ $this->params['breadcrumbs'][] = $event->name;
 $this->params['breadcrumbs'][] = $this->title;
 
 echo TreeView::widget([
-    // single query fetch to render the tree
-    // use the Product model you have in the previous step
     'query' => Section::find()->andWhere(['eventId' => $event->id])->addOrderBy('root, lft'),
     'rootOptions' => ['label'=>'<span class="text-primary">Секции</span>'],
     'headingOptions' => ['label' => 'Секции'],
-    'topRootAsHeading' => true, // this will override the headingOptions
-    'fontAwesome' => false,     // optional
-    'isAdmin' => false,         // optional (toggle to enable admin mode)
-    'displayValue' => 1,        // initial display value
-    'softDelete' => true,       // defaults to true
+    'topRootAsHeading' => true,
+    'fontAwesome' => false,
+    'isAdmin' => false,
+    'displayValue' => 1,
+    'softDelete' => true,
     'cacheSettings' => [
-        'enableCache' => false   // defaults to true
+        'enableCache' => false
     ],
     'nodeAddlViews' => [
         Module::VIEW_PART_2 => '@backend/views/sections/_type'
