e7841b9c2b5187b7f863270f6b8116ca60d14c58
deltamc <biomars@yandex.ru>
2018-05-24 07:51:36 +0300

    Generation model Teacher

---
+++ backend/models/Teacher.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%teachers}}".
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $participantId
+ *
+ * @property Participants $participant
+ */
+class Teacher extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%teachers}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'participantId'], 'required'],
+            [['participantId'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+            [['participantId'], 'exist', 'skipOnError' => true, 'targetClass' => Participants::className(), 'targetAttribute' => ['participantId' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'participantId' => 'Participant ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getParticipant()
+    {
+        return $this->hasOne(Participants::className(), ['id' => 'participantId']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return TeachersQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new TeachersQuery(get_called_class());
+    }
+}
